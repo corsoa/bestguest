5418172639b6960b68bfc4dfc62b9b3e7e66b5e7
Aaron Corso <aaron@corsoa.com>
2018-04-28 10:15:38 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,8 @@
+import {MDCRipple} from '@material/ripple';
+const ripple = new MDCRipple(document.querySelector('.foo-button'));
+
+import {MDCTopAppBar} from '@material/top-app-bar/index';
+
+// Instantiation
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
