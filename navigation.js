ca5cbaa3f0541f1da1dd8eaa13fa46a79e7befa8
Aaron Corso <aaron@corsoa.com>
2018-04-28 20:37:33 -0400

    Hooked up more UI logic

--- navigation.js
+++ navigation.js
@@ -1,4 +1,38 @@
 $(document).on('ready', (() => {
+  const generateFakeName = ((customer) => {
+    const initialMap = {
+      A: ['Aaron', 'Aimee', 'Amanda', 'Alexander'],
+      B: ['Bernard', 'Barbara', 'Brittany', 'Beth'],
+      C: ['Chrstina', 'Cathy', 'Christopher'],
+      D: ['David', 'Deborah'],
+      E: ['Ernie', 'Edith'],
+      F: ['Frances', 'Frank'],
+      G: ['Gerry'],
+      H: ['Harold', 'Harry'],
+      I: ['Irene'],
+      J: ['Jeff', 'John'],
+      K: ['Keith', 'Kristen'],
+      L: ['Lorraine'],
+      M: ['Maria'],
+      N: ['Nate'],
+      O: ['Oprah'],
+      P: ['Perry'],
+      Q: ['Quinn'],
+      R: ['Roger'],
+      S: ['Stacey', 'Stephen'],
+      T: ['Trevor'],
+      U: ['Utarr'],
+      V: ['Vishnu'],
+      W: ['Winston'],
+      X: ['Xavier'],
+      Y: ['Yvyonne'],
+      Z: ['Zack']
+    };
+    const getRandomName = ((initial) => {
+      return initialMap[initial][Math.floor(Math.random()*initialMap[initial].length)];
+    });
+    return `${getRandomName(customer.FirstInitial)} ${customer.LastName}`;
+  });
   /*
   const topCustomerData = [
     {
@@ -29,7 +63,8 @@ $(document).on('ready', (() => {
     if (row.Amount > 0) {
       if (!customerHash[row.FirstInitial + row.LastName]) {
         customerHash[row.FirstInitial + row.LastName] = {
-          cname: row.FirstInitial + ' ' + row.LastName,
+          FirstInitial: row.FirstInitial,
+          LastName: row.LastName,
           totalSpend: row.Amount,
           visits: 1
         }
@@ -72,7 +107,7 @@ $(document).on('ready', (() => {
 
   for (var customer in customerHash) {
     topCustomerData.push({
-      cname: customerHash[customer].cname,
+      cname: generateFakeName(customerHash[customer]),
       visits: customerHash[customer].visits,
       avgSpend: (customerHash[customer].totalSpend / customerHash[customer].visits).toFixed(2),
       totalSpend: (customerHash[customer].totalSpend).toFixed(2),
@@ -81,7 +116,6 @@ $(document).on('ready', (() => {
       score: calculateScore(customerHash[customer])
     });
   }
-  
 
   $('#top-customer-datatable').dataTable({
     data: topCustomerData,
@@ -132,24 +166,48 @@ $(document).on('ready', (() => {
         targets: 6
       }
     ]
-  })
- }));
- 
-  
-  
-  window.navPage = ((pageId) => {
-    $('#regular-customer-card').hide();
-    $('#top-customer-report-card').hide();
-    "8422",
-    "8422",
-    $('#intro-card').hide();
-
+  });
+  const getRandomCustomer = (() => {
+    return topCustomerData[Math.floor(Math.random()*topCustomerData.length)];
+  });
+  const buildRandomNotes = (() => {
+    const allNotes = ['Likes salsa music', 'Comes in every Tuesday', 'Diet coke, no ice', 'Vegetarian', 'Refers lots of business to us'];
+    return `<ul><li>${allNotes[Math.floor(Math.random() * allNotes.length)]}</li>
+    <li>${allNotes[Math.floor(Math.random() * allNotes.length)]}</li></ul>`;
+  });
+  window.navPage = ((pageId, newCustomer) => {
     if (!pageId) {
       $('#intro-card').show();
+      $('#regular-customer-card').hide();
+      $('#top-customer-report-card').hide();
     }
     else {
+      if (pageId === 'regular-customer') {
+        // put some data in.
+        const randomCustomer = getRandomCustomer();
+        $('#customer-data-name').html(randomCustomer.cname);
+        $('#customer-data-is-frequent-flier').html('-');
+        $('#customer-data-is-big-fish').html('-');
+        $('#customer-data-notes').html('');
+        if (!newCustomer) {
+          if (randomCustomer.isFrequentFlyer) {
+            $('#customer-data-is-frequent-flier').html('<i class="material-icons">airplanemode_active</i>');
+          }
+          if (randomCustomer.isBigFish) {
+            $('#customer-data-is-big-fish').html('<i class="material-icons">card_travel</i>');
+          }
+          if (randomCustomer.visits > 3) {
+            $('#customer-data-notes').html(buildRandomNotes());
+          }
+        }
+      }
+      else {
+        $('#regular-customer-card').hide();
+      }
       $('#' + pageId + '-card').show();
+      $('#intro-card').slideUp();
     }
   });
   navPage();
+ }));
 }));
